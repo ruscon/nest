62ba48858946c5cd6b03358d1ae5ec077a28eba9
Coroliov Oleg <coroliov.o@goparrot.ai>
2020-02-28 13:30:34 +0200

    feat(microservices): add IncomingKafkaMessage interface

--- packages/microservices/interfaces/index.ts
+++ packages/microservices/interfaces/index.ts
@@ -3,6 +3,7 @@ export * from './client-metadata.interface';
 export * from './closeable.interface';
 export * from './custom-transport-strategy.interface';
 export * from './deserializer.interface';
+export * from './incoming-kafka-message.interface';
 export * from './message-handler.interface';
 export * from './microservice-configuration.interface';
 export * from './packet.interface';
